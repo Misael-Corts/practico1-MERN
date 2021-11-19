e6b46c133a3ae418e137b939c6c3c514c1762048
Misael <misael.cortes39@gmail.com>
2021-11-19 20:03:28 -0300

    Exercise 1

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const countries = require('./countries.json');
+const countries = require("./countries.json");
 
 /** Get all the data of one single country given the ISO3 code.
  * @param  {string} isoCode
  * @returns {Object}
  */
-function getCountryByIso3(isoCode){
-    //Complete with your code
+function getCountryByIso3(isoCode) {
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].iso3 == isoCode) {
+      return countries[i];
+    }
+  }
 }
 
 /**
@@ -14,31 +18,25 @@ function getCountryByIso3(isoCode){
  * @param  {string} language example: "es"
  * @returns {string}
  */
-function getCountryTranslatedName(isoCode, language){
-    //Complete with your code
-}
+function getCountryTranslatedName(isoCode, language) {}
 /**Get an array of all the countries with the specified subregion
  * @param  {string} subregion
  * @returns {Array}
  */
-function getCountriesBySubregion(subregion){
-    //Complete with your code
-}
+function getCountriesBySubregion(subregion) {}
 
 function main() {
-    console.log(
-        '///// Ejercicio 1 /////\n',
-        getCountryByIso3('ARG'),
-        '\n');
-    console.log(
-        '///// Ejercicio 2 /////\n',
-        getCountryTranslatedName('ARG', 'ja'),
-        '\n');
-    console.log(
-        '///// Ejercicio 3 /////\n',
-        getCountriesBySubregion('South America'),
-        '\n');
-
+  console.log("///// Ejercicio 1 /////\n", getCountryByIso3("ARG"), "\n");
+  console.log(
+    "///// Ejercicio 2 /////\n",
+    getCountryTranslatedName("ARG", "kr"),
+    "\n"
+  );
+  console.log(
+    "///// Ejercicio 3 /////\n",
+    getCountriesBySubregion("Southern Asia"),
+    "\n"
+  );
 }
 
 main();
